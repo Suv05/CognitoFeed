2bcd518dbaab1f8274ea763e3240e6c849f4a201
suvam <suvampravu@gmail.com>
2025-07-29 21:17:04 +0530

    Home page ui-ux

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,7 +1,13 @@
-
-
+import Main from "@/components/home/main"
 export default function Home() {
   return (
-    <div><H1>Hello World</H1></div>
-  );
+    <>
+      {/* <Main /> */}
+      <Main></Main>
+      {/* <div>
+        <h1 className="text-sm font-bold text-slate-700 m-auto pt-10">hello world</h1>
+      </div> */}
+
+    </>
+  )
 }
